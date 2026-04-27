170f38c77a85763aab62ce2308e3c2356558f8fb
Amosh singh <amoshrana4thansingh@gmail.com>
2026-04-28 04:52:38 +0530

    model registered

--- payout_engine/core/admin.py
+++ payout_engine/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Merchant, Payout
+
+admin.site.register(Merchant)
+admin.site.register(Payout)
